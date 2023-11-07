89a6e7261c52b78ef8e6857cd034e5474170bb90
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-11-07 17:52:12 -0300

    feat: added new styles and components

---
+++ src/utils/Reducer.js
@@ -0,0 +1,45 @@
+import { reducerCases } from "./Constants";
+
+export const initialState = {
+  token: null,
+  playlists: [],
+  userInfo: null,
+  selectedPlaylistId: "3rTFrGTTnmgu1pRtQ6Hsnu",
+  selectedPlaylist: null,
+  currentlyPlaying: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case reducerCases.SET_PLAYLISTS: {
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    }
+    case reducerCases.SET_USER: {
+      return {
+        ...state,
+        userInfo: action.userInfo,
+      };
+    }
+    case reducerCases.SET_PLAYLIST: {
+      return {
+        ...state,
+        selectedPlaylist: action.selectedPlaylist,
+      };
+    }
+    case reducerCases.SET_PLAYING:
+      return {
+        ...state,
+        currentlyPlaying: action.currentlyPlaying,
+      };
+    default:
+      return {
+        ...state,
+        token: action.token,
+      };
+  }
+};
+
+export default reducer;
